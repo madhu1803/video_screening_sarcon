e3cededb8141ec1f47ece190c428842dd0920693
madhumithaa <madhumithaa1411@gmail.com>
2020-06-22 11:14:23 +0530

    desktop view responsive and sidebar and tabbar yet to be done

--- assets/js/video_stream.js
+++ assets/js/video_stream.js
@@ -1,19 +1,27 @@
 toggleAnnouncementsModal();
 // Notification Modal
 $("#minimize-btn").click(function () {
-    $(".notification-modal-class").toggleClass("d-none");
+  $(".notification-modal-class").toggleClass("d-none");
 });
 
 //on load minimize notification modal
 $("#minimize-btn").click();
 
 function toggleAnnouncementsModal() {
-    $("#notification-modal-dialog").toggleClass("d-none");
+  $("#notification-modal-dialog").toggleClass("d-none");
 }
 
 window.addEventListener("resize", function () {
-    width = $(window).width()
-    if (width <= 992) {
-        $('#chatModal .close').click()
-    }
+  width = $(window).width();
+  if (width <= 992) {
+    $("#chatModal .close").click();
+  }
 });
+
+function display() {
+  $(".qa-btn").toggleClass("d-none");
+  $(".audience-btn").toggleClass("d-none");
+  $(".polling-btn").toggleClass("d-none");
+  $("#notification-btn").toggleClass("d-none");
+  $("#chat-modal-btn").toggleClass("d-none");
+}
